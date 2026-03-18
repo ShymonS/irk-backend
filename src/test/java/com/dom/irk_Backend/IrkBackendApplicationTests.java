fccb11790b243e390e16013d37b8bafd2e912b20
Witek <witoldwalis@gmail.com>
2026-03-18 16:47:06 +0100

    Pierwszy commit - start projektu backendowego (Spring Boot)

---
+++ src/test/java/com/dom/irk_Backend/IrkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dom.irk_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
